c8f6eeddd5cd3c81b65a878c0eeaaa1540836526
Konstantin Shvyriaev <kshvyriaev.inc@gmail.com>
2020-06-15 18:06:41 +0400

    dateSample was fixed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity && typeof sampleActivity === 'string') {
+    const activity = parseFloat(sampleActivity);
+    if (!isNaN(activity) && activity > 0 && activity < 15) {
+      const k = 0.693 / HALF_LIFE_PERIOD;
+      const result = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+      return result;
+    }
+  }
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return false;
 };

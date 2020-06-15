691bc3b593fb0a9ed52927190f0b902e8cba9850
Konstantin Shvyriaev <kshvyriaev.inc@gmail.com>
2020-06-16 00:59:53 +0400

    chainMaker was fixed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(arguments.length === 0) {
+      this.chain.push('( )');
+    } else {
+      this.chain.push('( ' + value + ' )');
+    }
+
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position < 0 || position > this.chain.length - 1) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const resultChain = this.chain.join('~~');
+    this.chain = [];
+    return resultChain;
   }
 };
 

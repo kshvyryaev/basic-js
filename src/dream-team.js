c63a9575d8c718997a51fce40c99f705a0f90c36
Konstantin Shvyriaev <kshvyriaev.inc@gmail.com>
2020-06-15 18:08:10 +0400

    createDreamTeam was fixed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.flat) {
+    return false;
+  }
+
+  //return members.map(e => typeof e === 'string' ? e.trim().toUpperCase()[0] : '').sort().join('');
+
+  return members
+    .filter(x => typeof x === 'string')
+    .map(x => x.trim().toUpperCase()[0])
+    .sort()
+    .join('');
 };
